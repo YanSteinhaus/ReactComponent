81255695f9f555cc070df454ea0dc9600804f939
DIEGO RICARDO HORNBURG <diego.hornburg@alunos.sc.senac.br>
2024-08-13 21:23:55 -0300

    my first commit 13.08

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Equipe = (props) => {
+  return(
+    <div>
+       <Sobre nome={props.nome} atribuicao={props.atribuicao}/>
     </div>
-  );
+  )
+}
+ 
+const Sobre = (props) =>{
+  return(
+    <div>
+        <h2>Aluno: {props.nome}</h2><h3>Atribuicao: {props.atribuicao}</h3>
+    </div>
+  )
+}
+const Professor = (props) =>{
+  return(
+    <div>
+        <h2> Professores: {props.professor}</h2>
+    </div>
+  )
 }
 
+const ODS = (props) =>{
+  return(
+    <div>
+        <h2> Objetivo Desenvolvimento Sustentavel: {props.ods}</h2>
+    </div>
+  )
+}
+
+const Projeto = (props) =>{
+  return(
+    <div>
+        <h2> Projeto: {props.projeto}</h2>
+    </div>
+  )
+}
+ 
+function App(){
+  return(
+    <div className="container">
+        <h1>Equipe de Extensão ADS 4: </h1>
+        <ODS ods="9"/>
+        <Projeto projeto="Aplicativo Achados e Perdidos"/>
+        <Equipe nome="Diego" atribuicao="TechLead"/>
+        <Equipe nome="Enthoni" atribuicao="Dev. Frontend"/>
+        <Equipe nome="Mateus" atribuicao="Dev. Backend"/>
+        <Professor professor="Marcio e Karen"/>
+    </div>
+  )
+}
 export default App;
